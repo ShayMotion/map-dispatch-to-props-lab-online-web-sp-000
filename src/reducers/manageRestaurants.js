7b54e8284dc4c8ff7346ad23670cd2c4e544640e
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 16:50:08 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurants.js
+++ src/reducers/manageRestaurants.js
@@ -1,15 +1,14 @@
-export default function manageRestaurants(state = {
-  restaurants: []
-}, action) {
-  switch (action.type) {
+export default function manageRestaurants(state = {restaurants: []}, action) {
+    switch (action.type) {
 
-   case 'ADD_RESTAURANT':
-      return Object.assign({}, state, {
-        restaurants: state.restaurants.concat(action.restaurant)
-      });
+    case 'ADD_RESTAURANT':
+        return {
+            ...state,
+            restaurants: [...state.restaurants, action.restaurant]
+        }
 
     default:
-      return state;
+        return state;
 
-  }
+    }
 };
